c0bc674362e68363061e2efe26e9f3e9224ea4fa
Ning Yang <yangn0@qq.com>
2024-01-23 20:59:31 +0800

    feat: lanqiao-5129

--- lanqiao-5129/main.cpp
+++ lanqiao-5129/main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+int getZu(vector<int> h,int diff){
+    int n=h.size();
+    int zu=1;
+    int temp=h[0];
+    for(int i=1;i<n;i++){
+        if((h[i]-temp)>diff){
+            zu++;
+            temp=h[i];
+        }
+    }
+    return zu;
+}
+
 int main(){
     int n,k;cin>>n>>k;
     vector<int> h(n,0);
@@ -10,5 +23,21 @@ int main(){
     }
     sort(h.begin(),h.end());
 
+    int l=0;
+    int r=h[n-1]-h[0];
+    int mid=0;
+    int zu;
+    while(l<r){
+        mid=l+(r-l)/2;
+        zu=getZu(h,mid);
+        if(zu<=k) {
+            // diff 找大了
+            r=mid;
+        }else if(zu>k) {
+            // diff 找小了
+            l=mid+1;
+        }
+    }
+    cout<<l;
     return 0;
 }
